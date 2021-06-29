0da48285222e55294c3079e39686162d3e4d99cf
Dan Rice <dan.rice.92@outlook.com>
2021-06-29 13:25:09 -0600

    employment API

--- spec/requests/employment_spec.rb
+++ spec/requests/employment_spec.rb
@@ -10,6 +10,27 @@ describe "GET #index /employments" do
 
     expect(response.content_type).to eq("application/json; charset=utf-8")
     expect(response).to have_http_status(:ok)
-    expect(response.body).to eq({employments: [employment, second_employment]}.to_json)
+    expect(response.body).to eq({employments: [
+      { 
+        job_title: employment.title,
+        employer_name: employment.employer.name,
+        start_date: employment.start_date,
+        end_date: employment.end_date,
+        starting_pay: employment.starting_pay,
+        ending_pay: employment.ending_pay,
+        career_title: employment.career.title,
+        location: employment.locations.first
+      },
+      {
+        job_title: second_employment.title,
+        employer_name: second_employment.employer.name,
+        start_date: second_employment.start_date,
+        end_date: second_employment.end_date,
+        starting_pay: second_employment.starting_pay,
+        ending_pay: second_employment.ending_pay,
+        career_title: second_employment.career.title,
+        location: second_employment.locations.first
+      }
+    ]}.to_json)
   end
 end
