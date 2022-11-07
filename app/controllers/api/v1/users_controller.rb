cae36d13deaf70141eff504166d5168228c1ac90
Dan Rice <dan.rice.92@outlook.com>
2022-11-07 08:11:31 -0700

    another testing route

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,19 @@
 module API::V1
   class UsersController < APIController
+    def index
+      @user = User.first
+      render json: {
+        user: {
+          id: @user.id,
+          authentication_token: @user.encode_json_web_token,
+          email: @user.email,
+          email_verified: @user.email_verified,
+          first_name: @user.first_name,
+          last_name: @user.last_name
+        }
+      }
+    end
+
     def show
       @user = User.find(params[:id])
       render json: {user: @user}
@@ -12,7 +26,9 @@ module API::V1
         render json: {
           user: {
             id: @user.id,
+            authentication_token: @user.encode_json_web_token,
             email: @user.email,
+            email_verified: @user.email_verified,
             first_name: @user.first_name,
             last_name: @user.last_name
           }
