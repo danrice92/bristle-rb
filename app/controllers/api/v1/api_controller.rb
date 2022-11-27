6e9268f3cacc8ae21cf7114f50df618437963f0f
Dan Rice <dan.rice.92@outlook.com>
2022-11-26 23:02:47 -0700

    require_login before_action

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,4 +1,9 @@
 module API::V1
   class APIController < ApplicationController
+    before_action :require_login
+
+    def user_not_authorized
+      render json: {errors: {authorization: ["not allowed to perform this action"]}}, status: 403
+    end
   end
 end
